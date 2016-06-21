044d42f5c89d81a5869baf4c21efa582784b4c39
Romain Pelisse <belaran@gmail.com>
2016-06-21 16:25:22 +0200

    replace URL by simple rsync

--- testsuite-job.sh
+++ testsuite-job.sh
@@ -1,3 +1,5 @@
+/home/jboss/jenkins_workspace/jobs/eap-6.4.x-proposed-build/lastSuccessful/archive
+readonly JENKINS_HOME=${JENKINS_HOME:-'/home/jboss/jenkins_workspace/'}
 readonly JENKINS_URL=${1}
 readonly JOB_NAME=${2}
 readonly JENKINS_USERNAME=${3}
@@ -14,8 +16,10 @@ if [ -z "${JENKINS_PASSWORD}" ]; then
   exit 2
 fi
 
-
 if [ -z "${UPSTREAM_BUILD_URL}"  ]; then
+  rsync -Avrz "${JENKINS_HOME}/jobs/${PARENT_JOB_NAME}/lastSuccessful/archive/*" .
+else
+
   if [ -z "${JENKINS_URL}" ] ; then
     echo "Missing JENKINS_URL"
     exit 3
@@ -25,20 +29,16 @@ if [ -z "${UPSTREAM_BUILD_URL}"  ]; then
     echo "Missing JOB_NAME"
     exit 4
   fi
-  ARCHIVE_URL="${JENKINS_URL}/job/${PARENT_JOB_NAME}/lastSuccessfulBuild/artifact/*zip*/archive.zip"
-else
+
   ARCHIVE_URL="${UPSTREAM_BUILD_URL}/artifact/*zip*/archive.zip"
+
+  archive=$(mktemp)
+  wget --auth-no-challenge --user "${JENKINS_USERNAME}" --password "${JENKINS_PASSWORD}" -nv "${ARCHIVE_URL}" -O "${archive}"
+  unzip -q "${archive}" -d archive
+  rm "${archive}"
+  cd archive
+  mv * ..
 fi
-
-archive=$(mktemp)
-wget --auth-no-challenge --user "${JENKINS_USERNAME}" --password "${JENKINS_PASSWORD}" -nv "${ARCHIVE_URL}" -O "${archive}"
-unzip -q "${archive}" -d archive
-rm "${archive}"
-cd archive
-mv * ..
-cd ..
-rmdir archive
-
 
 . /opt/jboss-set-ci-scripts/common_bash.sh
 set_ip_addresses
