371cc793dc8b76be7f9460e3ff6d9c8fd41b51ee
Romain Pelisse <belaran@gmail.com>
2016-06-30 10:35:01 +0200

    fix user setting for container run, moved to the appropriate command

--- docker-job.sh
+++ docker-job.sh
@@ -19,7 +19,7 @@ readonly OLD_RELEASES_FOLDER=${OLD_RELEASES_FOLDER:-'/opt/old-as-releases'}
 
 readonly DOCKER_IMAGE=${DOCKER_IMAGE:-'rhel6-jenkins-shared-slave'}
 
-readonly CONTAINER_ID=$(docker run -d -v "${WORKSPACE}:/workspace"  -v "${MAVEN_HOME}:/maven_home" -v "${JAVA_HOME}:/java" -v "${OLD_RELEASES_FOLDER}:${OLD_RELEASES_FOLDER}" -v $(pwd):/job_home "${DOCKER_IMAGE}")
+readonly CONTAINER_ID=$(docker run -d -u ${USER} -v "${WORKSPACE}:/workspace"  -v "${MAVEN_HOME}:/maven_home" -v "${JAVA_HOME}:/java" -v "${OLD_RELEASES_FOLDER}:${OLD_RELEASES_FOLDER}" -v $(pwd):/job_home "${DOCKER_IMAGE}")
 
 if [ "${?}" -ne 0 ]; then
   echo 'Failed to create container.'
@@ -34,7 +34,7 @@ if [ -z "${CONTAINER_ID}" ]; then
 fi
 
 trap "docker stop ${CONTAINER_ID}" EXIT INT QUIT TERM
-docker exec -u "${USER}" "${CONTAINER_ID}" /bin/bash /job_home/job-run.sh
+docker exec "${CONTAINER_ID}" /bin/bash /job_home/job-run.sh
 status=${?}
 docker stop "${CONTAINER_ID}"
 exit "${status}"
