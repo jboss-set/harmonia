1ca04e3ccba2910e9def8b18a75772a395c130b6
Romain Pelisse <belaran@gmail.com>
2016-08-26 14:35:48 +0200

    set up JAVA_HOME and MAVEN_HOME - if missing, before firing the all integration test

--- all-integration-tests.sh
+++ all-integration-tests.sh
@@ -3,13 +3,30 @@
 # Run all integration test from Widlfly/EAP
 #
 # Allow to override the following values:
-readonly LOCAL_REPO_DIR=${LOCAL_REPO_DIR:-$WORKSPACE/maven-local-repository}
+readonly LOCAL_REPO_DIR=${LOCAL_REPO_DIR:-"${WORKSPACE}/maven-local-repository"}
 readonly OLD_RELEASES_FOLDER=${OLD_RELEASES_FOLDER:-'/opt/old-as-releases'}
 readonly MEMORY_SETTINGS=${MEMORY_SETTINGS:-'-Xmx1024m -Xms512m -XX:MaxPermSize=256m'}
 readonly SUREFIRE_FORKED_PROCESS_TIMEOUT=${SUREFIRE_FORKED_PROCESS_TIMEOUT:-'90000'}
 readonly MAVEN_IGNORE_TEST_FAILURE=${MAVEN_IGNORE_TEST_FAILURE:-'false'}
-# and will reuse MAVEN_OPTS if defined.
+readonly DEFAULT_MAVEN_HOME="$(pwd)/tools/maven"
+# and will reuse MAVEN_OPTS and TESTSUITE_OPTS if defined.
 
+echo "JAVA_HOME: ${JAVA_HOME}"
+if [ -n "${JAVA_HOME}" ]; then
+   export PATH=${JAVA_HOME}/bin:${PATH}
+   java -version
+fi
+
+if [ -n "${MAVEN_HOME}" ]; then
+  echo "No Maven Home defined - setting to default: ${DEFAULT_MAVEN_HOME}"
+  export MAVEN_HOME=${DEFAULT_MAVEN_HOME}
+  cd $(pwd)/tools
+  bash ./download-maven.sh
+  cd -
+  export PATH=${MAVEN_HOME}/bin:${PATH}
+  which mvn
+  mvn -version
+fi
 
 if [ ! -z "${EXECUTOR_NUMBER}" ]; then
   echo -n "Job run by executor ID ${EXECUTOR_NUMBER} "
