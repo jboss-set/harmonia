bd9bae1ffb6aa168482efe5f6b0a7996918f8640
Pelisse Romain <belaran@redhat.com>
2019-08-23 13:32:45 +0200

    Makes path to alignment jar configurable

--- wildfly-upgrade-components.sh
+++ wildfly-upgrade-components.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -e
 
+usage() {
+
+  echo $(basename "${0}") [path-to-alignment-jar]
+}
+
 readonly JBOSS_USER_HOME='/home/jboss'
-readonly CLI="${JBOSS_USER_HOME}/alignment-cli-0.3-SNAPSHOT.jar"
+readonly CLI="${1:-${JBOSS_USER_HOME}/alignment-cli-0.3-SNAPSHOT.jar}"
 readonly CONFIG="${JBOSS_USER_HOME}/wildfly-18-alignment-config.json"
 
 set -u
