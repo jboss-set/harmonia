b3cbe93452000293d2708d3cafc8c5924ce1c598
Romain Pelisse <belaran@gmail.com>
2019-10-02 10:27:11 +0200

    Add testsuite script for harmonia-ci job

---
+++ tests/testsuite.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "Run Tests..."
+bats -t tests/eap-job-tests.bats > eap.tap
+bats -t tests/eat-job-tests.bats > eat.tap
+bats -t tests/upgrade-components-report-test.sh > upgrade-components-report.tap
+cd perun || exit 1
+bats -t tests/run-test-unit-tests.bats > ../perun-run-test.tap
+bats -t tests/perun-unit-tests.bats > ../perun-unit-tests.tap
+cd .. || exit 1
+echo 'Done.'
+echo ''
+echo 'Run Shellcheck on scripts...'
+for script_file in *.sh
+do
+  echo "===== ${script_file} ===="
+  shellcheck -e SC2086 -e SC2068 -e SC2181 "${script_file}"
+done
+echo 'Done.'
