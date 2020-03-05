aaf0925154b43aeea9423b07b89d3a73b8ef8f47
Romain Pelisse <belaran@gmail.com>
2020-03-05 13:35:12 +0100

    Remove duplicated logic with eap-job.sh

--- eap-job-cci-eap64.sh
+++ eap-job-cci-eap64.sh
@@ -1,35 +1,14 @@
-# Allow to override the following values:
-readonly EAP_LOCAL_MAVEN_REPO_FOLDER=${EAP_LOCAL_MAVEN_REPO_FOLDER:-eap-maven-local-repository}
-readonly EAP_LOCAL_MAVEN_REPO=${EAP_LOCAL_MAVEN_REPO:-${WORKSPACE}/${EAP_LOCAL_MAVEN_REPO_FOLDER}}
-readonly MEMORY_SETTINGS=${MEMORY_SETTINGS:-'-Xmx1024m -Xms512m -XX:MaxPermSize=256m'}
-readonly SRC_LOCATION_FOLDER=${SRC_LOCATION_FOLDER:-eap-sources}
-readonly SRC_LOCATION=${SRC_LOCATION:-${WORKSPACE}/${SRC_LOCATION_FOLDER}}
+#!/bin/bash
+set -e
 
-# and will reuse MAVEN_OPTS and TESTSUITE_OPTS if defined.
-mkdir -p "${EAP_LOCAL_MAVEN_REPO}"
-export MAVEN_OPTS="${MAVEN_OPTS} ${MEMORY_SETTINGS}"
-export MAVEN_OPTS="${MAVEN_OPTS} -Dmaven.repo.local=${EAP_LOCAL_MAVEN_REPO}"
-
-# Remove home variable
-unset JBOSS_HOME
-
-cd ${SRC_LOCATION}
-echo "Starting build..."
-./build.sh clean install -fae -B -DskipTests -Dts.noSmoke -DallTests -Prelease
-
-# Adjust Surefire memory settings (due to failures in JDK6)
-echo "Adjusted memory settings"
+echo 'Adjust Surefire memory settings (due to failures in JDK6)'
 sed -i 's/-Duser.language=en<\/argLine>/-Duser.language=en -XX:MaxPermSize=256m<\/argLine>/g' pom.xml
 
-echo "Finished build"
-cd ${WORKSPACE}
+echo -n "Building release artifacts..."
 
-# Make all artifacts
-# Maven artifacts
-zip -qr jboss-eap-6.4-maven-artifacts.zip ${EAP_LOCAL_MAVEN_REPO_FOLDER}
-
-# Server and sources
-echo "Copying distribution artifacts to workspace"
-#mv -v ${SRC_LOCATION}/dist/target/jboss-eap-6.4.zip ${WORKSPACE}/
-#mv -v ${SRC_LOCATION}/dist/target/jboss-eap-6.4-src.zip ${WORKSPACE}/
-zip -qr jboss-eap-6.4-src-prepared.zip ${SRC_LOCATION_FOLDER}
+readonly EAP_RELEASE_NAME=${EAP_RELEASE_NAME:-'jboss-eap-6.4'}
+readonly EAP_MAVEN_ARTIFACTS_ZIPFILE_NAME=${EAP_MAVEN_ARTIFACTS_ZIPFILE_NAME:-"${EAP_RELEASE_NAME}-maven-artifacts.zip"}
+readonly EAP_MAVEN_SOURCES_ZIPFILE_NAME=${EAP_MAVEN_ARTIFACTS_ZIPFILE_NAME:-"${EAP_RELEASE_NAME}-src-prepared.zip"}
+zip -qr "${EAP_MAVEN_ARTIFACTS_ZIPFILE_NAME}" ${EAP_LOCAL_MAVEN_REPO_FOLDER} 2>&1 > /dev/null
+zip -qr "${EAP_MAVEN_SOURCES_ZIPFILE_NAME}" ${WORKSPACE} 2>&1 > /dev/null
+echo 'Done.'
