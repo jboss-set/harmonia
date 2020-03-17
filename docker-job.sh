2b6ab93842a1dbbb53594a82977f1f88b02fb7af
Romain Pelisse <belaran@gmail.com>
2020-03-17 12:57:48 +0100

    fixup

--- docker-job.sh
+++ docker-job.sh
@@ -4,7 +4,6 @@ readonly DOCKER_CMD=${DOCKER_CMD:-'/usr/bin/docker'}
 readonly ROOT_CMD_FOR_DOCKER_CONTAINER=${ROOT_CMD_FOR_DOCKER_CONTAINER:-'/bin/bash'}
 readonly DOCKER_IMAGE=${DOCKER_IMAGE:-'rhel6-jenkins-shared-slave'}
 readonly WORKSPACE_MOUNT_POINT=${WORKSPACE_MOUNT_POINT:-'/work/'}
-readonly ENV_FILE=$(mktemp)
 
 docker_cmd() {
   local cmd=${1}
@@ -32,15 +31,11 @@ cleanUpContainer() {
     killContainer
   fi
 
-  if [ -e "${ENV_FILE}" ]; then
-    rm -f "${ENV_FILE}"
-  fi
 }
 
 readonly REAL_WORKSPACE=${WORKSPACE}
 export WORKSPACE=${WORKSPACE_MOUNT_POINT}
 
-env > "${ENV_FILE}"
-readonly CONTAINER_ID=$("${DOCKER_CMD}" run --env-file="${ENV_FILE}" -v "${REAL_WORKSPACE}:${WORKSPACE_MOUNT_POINT}:rw"  -v '/opt:/opt:ro' -v '/home/jboss:/home/jboss:ro' -tdi --privileged "${DOCKER_IMAGE}" "${ROOT_CMD_FOR_DOCKER_CONTAINER}")
+readonly CONTAINER_ID=$("${DOCKER_CMD}" run -e MAVEN_HOME="${MAVEN_HOME}" -v "${REAL_WORKSPACE}:${WORKSPACE_MOUNT_POINT}:rw"  -v '/opt:/opt:ro' -v '/home/jboss:/home/jboss:ro' -tdi --privileged "${DOCKER_IMAGE}" "${ROOT_CMD_FOR_DOCKER_CONTAINER}")
 trap cleanUpContainer EXIT
 docker exec -t "${CONTAINER_ID}" "${PATH_TO_SCRIPT}"
