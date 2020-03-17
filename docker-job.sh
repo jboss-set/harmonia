757707857fc6d689bbbe0814d9d2368b0d9da028
Romain Pelisse <belaran@gmail.com>
2020-03-17 12:10:28 +0100

    Ensure WORKSPACE is being set to associate mount point

--- docker-job.sh
+++ docker-job.sh
@@ -3,6 +3,7 @@
 readonly DOCKER_CMD=${DOCKER_CMD:-'/usr/bin/docker'}
 readonly ROOT_CMD_FOR_DOCKER_CONTAINER=${ROOT_CMD_FOR_DOCKER_CONTAINER:-'/bin/bash'}
 readonly DOCKER_IMAGE=${DOCKER_IMAGE:-'rhel6-jenkins-shared-slave'}
+readonly WORKSPACE_MOUNT_POINT=${WORKSPACE_MOUNT_POINT:-'/work/'}
 readonly ENV_FILE=$(mktemp)
 
 docker_cmd() {
@@ -36,7 +37,10 @@ cleanUpContainer() {
   fi
 }
 
+readonly REAL_WORKSPACE=${WORKSPACE}
+export WORKSPACE=${WORKSPACE_MOUNT_POINT}
+
 env > "${ENV_FILE}"
-readonly CONTAINER_ID=$("${DOCKER_CMD}" run --env-file="${ENV_FILE}" -v "$(pwd)":/work/:rw  -v '/opt:/opt:ro' -tdi --privileged "${DOCKER_IMAGE}" "${ROOT_CMD_FOR_DOCKER_CONTAINER}")
+readonly CONTAINER_ID=$("${DOCKER_CMD}" run --env-file="${ENV_FILE}" -v "${REAL_WORKSPACE}:${WORKSPACE_MOUNT_POINT}:rw"  -v '/opt:/opt:ro' -tdi --privileged "${DOCKER_IMAGE}" "${ROOT_CMD_FOR_DOCKER_CONTAINER}")
 trap cleanUpContainer EXIT
 docker exec -t "${CONTAINER_ID}" "${PATH_TO_SCRIPT}"
