da318b26d437302acd0b4a751056910c5e8cca03
Romain Pelisse <belaran@gmail.com>
2021-03-09 14:50:37 +0100

    Delete deprecated scripts

--- upgrade-components-report.sh
+++ upgrade-components-report.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
-set -e
+set -ex
 
 usage() {
-  echo "$(basename "${0}") [email] [rule-name] [target-dir] [report-title]"
+  echo "$(basename "${0}") [email] [rule-name] [target-dir] [report-title] [project-code]"
 }
 
-deleteOldReportFile() {
-  if [ -e "${REPORT_FILE}" ]; then
-    echo "Deleting ${REPORT_FILE}"
-    rm "${REPORT_FILE}"
-  fi
+emailWithSMTP() {
+    mutt -e "set from = '${FROM_ADDRESS}'"
+         -e 'set content_type=text/html' \
+         -s "Possible component upgrades report - ${REPORT_TITLE}" \
+         "${TO_ADDRESS}" < "${REPORT_FILE}"
 }
 
-readonly EMAIL="${1}"
+emailWithGMail() {
+    mutt -e 'set content_type = text/html' \
+         -e "set smtp_url = 'smtps://${FROM_ADDRESS}@smtp.gmail.com:465'" \
+         -e "set smtp_pass = '${SMTP_PASSWORD}'" \
+         -e "set ssl_starttls = yes" \
+         -e "set ssl_force_tls = yes" \
+         -s "Possible component upgrades report - ${REPORT_TITLE}" \
+         "${TO_ADDRESS}" < "${REPORT_FILE}"
+}
 
-if [ -z "${EMAIL}" ]; then
+readonly TO_ADDRESS="${1}"
+
+if [ -z "${TO_ADDRESS}" ]; then
   echo 'Missing email adress.'
   usage
   exit 1
 fi
 
-if [ "${EMAIL}" = '-h' ]; then
+if [ "${1}" = '-h' ]; then
   usage
   exit 0
 fi
@@ -42,13 +52,20 @@ if [ -z "${TARGET_DIR}" ]; then
 fi
 
 readonly REPORT_TITLE="${4:-$(basename "${TARGET_DIR}")}"
+readonly LOGGER_PROJECT_CODE="${5}"
 
 readonly JBOSS_USER_HOME=${JBOSS_USER_HOME:-'/home/jboss'}
 readonly CLI="${JBOSS_USER_HOME}/alignment-cli.jar"
 readonly CONFIG=${CONFIG:-"${JBOSS_USER_HOME}/dependency-alignment-configs/rules-${RULE_NAME}.json"}
 readonly TARGET="${TARGET_DIR}/pom.xml"
-readonly REPORT_FILE=${REPORT_FILE:-'report.txt'}
+readonly REPORT_FILE=${REPORT_FILE:-'report.html'}
 readonly FROM_ADDRESS=${FROM_ADDRESS:-'thofman@redhat.com'}
+readonly LOGGER_URI=${LOGGER_URI:-'http://component-upgrade-logger-jvm-component-alignment.int.open.paas.redhat.com/api'}
+
+readonly GMAIL_SMTP_PASSWORD_FILE=${GMAIL_SMTP_PASSWORD_FILE:-"${HOME}/.gmail-smtp-password.gpg"}
+if [ -e "${GMAIL_SMTP_PASSWORD_FILE}" ]; then
+  readonly SMTP_PASSWORD=$(gpg -d "${GMAIL_SMTP_PASSWORD_FILE}" 2> /dev/null)
+fi
 
 set -u
 
@@ -61,13 +78,23 @@ echo '==== REPORT CONFIGURATION ==='
 cat "${CONFIG}"
 echo '===='
 
-deleteOldReportFile
+# delete old report file
+if [ -e "${REPORT_FILE}" ]; then
+  echo "Deleting ${REPORT_FILE}"
+  rm "${REPORT_FILE}"
+fi
 
-java -jar "${CLI}" 'generate-report' -c "${CONFIG}" -f "${TARGET}" -o "${REPORT_FILE}"
+java -Dlogger.projectCode="${LOGGER_PROJECT_CODE}" \
+     -Dlogger.uri="${LOGGER_URI}" \
+     -jar "${CLI}" 'generate-html-report' \
+     -c "${CONFIG}" -f "${TARGET}" -o "${REPORT_FILE}"
 
 if [ -e "${REPORT_FILE}" ] && [ -s "${REPORT_FILE}" ]; then
-    mail -s "Possible component upgrades report - ${REPORT_TITLE}" -r "${FROM_ADDRESS}" "${EMAIL}" < "${REPORT_FILE}"
+  if [ -z "${SMTP_PASSWORD}" ]; then
+    emailWithSMTP
+  else
+    emailWithGMail
+  fi
 else
     echo "No report generated"
 fi
-
