5fe4c61405a72ee3b3f61a0f34a3bbff6205633c
Bartosz Spyrko-Smietanko <bspyrkos@redhat.com>
2021-03-15 16:50:06 +0000

    [SET-334] Adding UMB message generation script

--- tests/eap-job-tests.bats
+++ tests/eap-job-tests.bats
@@ -12,6 +12,9 @@ setup() {
   export WORKSPACE=$(mktemp -d)
 
   echo 'echo @{@}' > "${WORKSPACE}/${INTEGRATION_TESTS_SCRIPT}"
+
+  # run tests within workspace
+  cd "${WORKSPACE}"
 }
 
 teardown() {
@@ -19,6 +22,7 @@ teardown() {
   deleteIfExist "${JBOSS_FOLDER}"
   deleteIfExist "${WORKSPACE}"
   deleteIfExist "${WORKSPACE}/${INTEGRATION_TESTS_SCRIPT}"
+  deleteIfExist "${WORKSPACE}/eap-sources"
   unset MAVEN_HOME
   unset JBOSS_FOLDER
 
@@ -35,18 +39,28 @@ teardown() {
   local extra_arg='-Dsome.extra.arg'
   run "${SCRIPT}" 'build' "${extra_arg}"
   [ "${status}" -eq 0 ]
-  [ "${lines[${#lines[@]}-1]}" = "mvn clean install -fae -s /home/master/settings.xml -B ${extra_arg}" ]
+  [ "${lines[${#lines[@]}-1]}" = "mvn clean install -Dts.skipTests=true -fae -s /home/master/settings.xml -B ${extra_arg}" ]
 }
 
 @test "Run with default settings.xml" {
   run "${SCRIPT}"
   [ "${status}" -eq 0 ]
-  [ "${lines[${#lines[@]}-1]}" = 'mvn clean install -fae -s /home/master/settings.xml -B' ]
+  [ "${lines[${#lines[@]}-1]}" = 'mvn clean install -Dts.skipTests=true -fae -s /home/master/settings.xml -B' ]
 }
 
 @test "No settings.xml provided" {
   export MAVEN_SETTINGS_XML=''
   run "${SCRIPT}"
   [ "${status}" -eq 0 ]
-  [ "${lines[${#lines[@]}-1]}" = 'mvn clean install -fae -B' ]
+  [ "${lines[${#lines[@]}-1]}" = 'mvn clean install -Dts.skipTests=true -fae -B' ]
+}
+
+@test "Record build properties" {
+  export IS_CCI=true
+  mkdir ${WORKSPACE}/eap-sources
+  mkdir -p ${WORKSPACE}/eap-sources/dist/target/jboss-eap-1234
+
+  run "${SCRIPT}"
+  [ "${status}" -eq 0 ]
+  [ -e "${WORKSPACE}/umb-build.properties" ]
 }
---
+++ umb-build-message.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+readonly REQUIRED_VARS=(SERVER_URL BUILD_ID BUILD_URL SCM_URL SCM_REVISION RELEASE_NAME RELEASE_STREAM RELEASE_TYPE VERSION BASE_VERSION)
+
+if ! command -v jq &> /dev/null
+then
+    echo "jq could not be found"
+    exit 1
+fi
+
+MISSING_VARS=""
+for var in "${REQUIRED_VARS[@]}"
+do
+  if [ -z ${!var} ]; then
+    if [ -n "${MISSING_VARS}" ]; then
+      MISSING_VARS="${MISSING_VARS} ${var}"
+    else
+      MISSING_VARS="${var}"
+    fi
+  fi
+done
+
+if [ -n "${MISSING_VARS}" ]; then
+  echo "Required properties are not found: ${MISSING_VARS}"
+  exit 2
+fi
+
+server=$( jq -n '{ server: $ARGS.named }' \
+      --arg url $SERVER_URL \
+      --arg build-type "jenkins" \
+      --arg build-id $BUILD_ID \
+      --arg build-url $BUILD_URL \
+      --arg scm-type: "git" \
+      --arg scm-url: $SCM_URL \
+      --arg scm-revision: $SCM_REVISION )
+ 
+sources=$( jq -n '{ "server-name": $ARGS.named }' \
+      --arg url $SOURCE_URL \
+      --arg build-type "jenkins" \
+      --arg build-id $BUILD_ID \
+      --arg build-url $BUILD_URL \
+      --arg scm-type: "git" \
+      --arg scm-url: $SCM_URL \
+      --arg scm-revision: $SCM_REVISION )
+
+server=$( echo $server | jq '.server' )
+sources=$( echo $sources | jq '."server-name"' )
+
+bits=$( jq -n '{bits: $ARGS.named}' \
+  --argjson server "$server" --argjson server-sources "$sources" )
+bits=$( echo $bits | jq '.bits' )
+
+msg=$( jq -n '{release: $ARGS.named}' \
+  --arg name "$RELEASE_NAME" \
+  --arg stream "$RELEASE_STREAM" \
+  --arg type "$RELEASE_TYPE" \
+  --arg version "$VERSION" \
+  --arg target-version "$BASE_VERSION" \
+  --arg eap-base-version "$BASE_VERSION" \
+  --arg url "$BUILD_URL" \
+  --argjson bits "$bits" )
+
+echo $msg | jq > message_body.json
+
+echo MESSAGE_CONTENT=$msg >> MESSAGE.txt
+
+cat message_body.json
