02e5472aff9b79b9e0e6e9f4d7e453893343f905
Romain Pelisse <belaran@gmail.com>
2021-05-26 10:34:00 +0200

    Add homedir for component upgrade script

--- upgrade-components-report.sh
+++ upgrade-components-report.sh
@@ -96,7 +96,8 @@ ifRequestedPrintUsageAndExit "${1}"
 
 readonly DEBUG=${DEBUG:-true}
 readonly TARGET_DIR=${TARGET_DIR:-'.'}
-readonly CLI=${PATH_TO_CLI:-'/opt/tools/component_alignment/alignment-cli-0.7.jar'}
+readonly COMPONENT_ALIGNMENT_HOME=${COMPONENT_ALIGNMENT_HOME:-'/opt/tools/component-alignment'}
+readonly CLI=${PATH_TO_CLI:-"${COMPONENT_ALIGNMENT_HOME}/alignment-cli-0.7.jar"}
 readonly JOBS_SETTINGS=${JOBS_SETTINGS:-'/opt/tools/component-alignment-config-template.csv'}
 readonly REPORT_FILE=${REPORT_FILE:-'report.html'}
 readonly FROM_ADDRESS=${FROM_ADDRESS:-'thofman@redhat.com'}
@@ -126,7 +127,7 @@ readonly RULE_NAME=$(echo "${JOB_CONFIG}" | cut -f2 -d, )
 readonly REPORT_TITLE=$( echo "${JOB_CONFIG}" | cut -f3 -d, )
 readonly LOGGER_PROJECT_CODE=$(echo "${JOB_CONFIG}" | cut -f3 -d, )
 
-readonly CONFIG_HOME=${CONFIG_HOME:-'/opt/tools/component_alignment/dependency-alignment-configs'}
+readonly CONFIG_HOME=${CONFIG_HOME:-"${COMPONENT_ALIGNMENT_HOME}/dependency-alignment-configs/"}
 readonly CONFIG=${CONFIG:-"${CONFIG_HOME}/rules-${RULE_NAME}.json"}
 
 if [ ! -e "${CONFIG}" ]; then
