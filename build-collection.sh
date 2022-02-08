3d29f004700514d16009feac43c59b37c580e04b
Romain Pelisse <belaran@gmail.com>
2022-02-08 09:33:57 +0100

    Change for to while on shellcheck request

--- build-collection.sh
+++ build-collection.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 readonly GALAXY_YML='galaxy.yml'
 readonly UPSTREAM_NS='middleware_automation'
 readonly DOWNSTREAM_NS='redhat'
-readonly PROJECT_DOWNSTREAM_NAME="${PROJECT_DOWNSTREAM_NAME:-''}"
+readonly PROJECT_DOWNSTREAM_NAME="${PROJECT_DOWNSTREAM_NAME}"
 readonly DEFAULT_UPSTREAM_GIT_BRANCH='main'
 
 echo GIT_REPOSITORY_URL: "${GIT_REPOSITORY_URL}"
@@ -22,7 +22,9 @@ if [ -n "${GIT_REPOSITORY_URL}" ]; then
 fi
 
 echo -n "Change collection namespace from ${UPSTREAM_NS} to ${DOWNSTREAM_NS}..."
-for file_to_edit in $(grep -e "${UPSTREAM_NS}" -r . | cut -f1 -d: | sort -u)
+grep -e "${UPSTREAM_NS}" -r . | cut -f1 -d: | sort -u | \
+while
+  read -r file_to_edit
 do
   echo -n "Editing ${file_to_edit}..."
   sed -i "${file_to_edit}" \
