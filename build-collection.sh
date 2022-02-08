776aa9894d66f8e1261b7829c712cba34b7dc682
Romain Pelisse <belaran@gmail.com>
2022-02-08 09:15:43 +0100

    Removing pager in git diff to avoid blocking build

--- build-collection.sh
+++ build-collection.sh
@@ -12,6 +12,7 @@ echo GIT_REPOSITORY_BRANCH: "${GIT_REPOSITORY_BRANCH}"
 echo WORKDIR: "${WORKDIR}"
 echo VERSION: "${VERSION}"
 
+echo "Building project from ${GIT_REPOSITORY_URL} inside ${WORKDIR}."
 cd "${WORKDIR}"
 if [ -n "${GIT_REPOSITORY_URL}" ]; then
   echo "Syncronizing with upstream ${GIT_REPOSITORY_URL} repository..."
@@ -29,7 +30,7 @@ do
    echo 'Done.'
 done
 
-if [ -n "${PROJECT_DOWNSTREAM_NAME}" ]; then
+if [ "${PROJECT_DOWNSTREAM_NAME}" != "" ]; then
   echo "Change collection name to ${PROJECT_DOWNSTREAM_NAME}..."
   sed -i "${GALAXY_YML}" -e "s/\(^name: \).*$/\1${PROJECT_DOWNSTREAM_NAME}/"
   echo 'Done.'
@@ -43,8 +44,13 @@ if [ -n "${VERSION}" ]; then
   echo 'Done.'
 fi
 
-git diff --no-color .
+echo 'Display changes performed on code base:'
+git --no-pager diff --no-color -w .
+echo 'Done'
+
+echo 'Build collection:'
 ansible-galaxy collection build .
+echo 'Done.'
 
 if [ -n "${VERSION}" ]; then
   echo "Tagging release ${TAG}"
