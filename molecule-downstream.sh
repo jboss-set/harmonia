2743697c0314dc2ba8922a6094236ae39118fc70
Romain Pelisse <belaran@gmail.com>
2022-07-27 16:53:37 +0200

    modownfixup

--- molecule-downstream.sh
+++ molecule-downstream.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
+
+readonly HARMONIA_HOME=${HARMONIA_HOME:-'harmonia'}
+
 if [ ! -d "${WORKDIR}" ]; then
   echo "WORKDIR ${WORKDIR} does not exists or is not a directory."
   exit 1
@@ -8,7 +11,5 @@ fi
 
 cd "${WORKDIR}"
 set +u
-./ansible-install-collections.sh
-
-set +u
-./molecule.sh
+${HARMONIA_HOME}/ansible-install-collections.sh
+${HARMONIA_HOME}/molecule.sh
