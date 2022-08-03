d81e686b7c97a64681106e200edfc75212cf2f46
Romain Pelisse <belaran@gmail.com>
2022-08-03 15:48:54 +0200

    Rework molecule downstream CI

--- molecule-downstream.sh
+++ molecule-downstream.sh
@@ -1,18 +1,45 @@
 #!/bin/bash
-set -euo pipefail
+set -eo pipefail
 
+copyCollectionFrom() {
+  local path_to_collection=${1}
+  local workdir=${WORKDIR:-"${2}"}
 
-readonly HARMONIA_HOME=${HARMONIA_HOME:-'harmonia'}
+  if [ -d "${path_to_collection}" ]; then
+    echo -n "Fetching last build from ${path_to_collection}..."
+    cp -r "${path_to_collection}"/* "${workdir}"
+    cp "${path_to_collection}/.ansible-lint" "${workdir}"
+    cp "${path_to_collection}/.yamllint" "${workdir}"
+    echo 'Done.'
+  else
+    echo "Invalid path to collection (does not exists or not a directory): ${path_to_collection}."
+  fi
+}
 
-if [ ! -d "${WORKDIR}" ]; then
-  echo "WORKDIR ${WORKDIR} does not exists or is not a directory."
+if [ -z "${WORKSPACE}" ]; then
+  echo "No WORKSPACE env var defined, aborting..."
   exit 1
 fi
 
-cd "${WORKDIR}"
+readonly HARMONIA_HOME=${HARMONIA_HOME:-"${WORKSPACE}/harmonia"}
 
-ls .
-echo OK
-set +u
-${HARMONIA_HOME}/ansible-install-collections.sh
-${HARMONIA_HOME}/molecule.sh
+if [ -z "${WORKDIR}" ]; then
+  echo "WORKDIR is not defined, aborting..."
+  exit 2
+fi
+
+mkdir -p "${WORKDIR}"
+cd "${WORKDIR}"
+if [ -n "${LAST_SUCCESS_FULL_BUILD_ID}" ]; then
+  echo "The LAST_SUCCESSFULL_BUILD_ID provied: ${LAST_SUCCESS_FULL_BUILD_ID}"
+  if [ -z "${PARENT_JOB_HOME}" ] ; then
+     echo "PARENT_JOB_HOME not provided and required for this kind of build, aborting..."
+     exit 3
+  fi
+  copyCollectionFrom "${PARENT_JOB_HOME}/builds/${LAST_SUCCESS_FULL_BUILD_ID}/archive/workdir/downstream/${PROJECT_NAME}"
+else
+  echo "TODO: a git checkout from gitlab"
+  exit 4
+fi
+
+"${HARMONIA_HOME}/molecule.sh"
