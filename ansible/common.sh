9db876029e5cc62aff4cceedfbb421cd5daaf51d
Romain Pelisse <belaran@gmail.com>
2023-08-03 18:04:23 +0200

    molecule: reorg internal libs

--- ansible/common.sh
+++ ansible/common.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 ANSIBLE_CONFIG=${ANSIBLE_CONFIG:-'/var/jenkins_home/ansible.cfg'}
+COLLECTIONS_REQ=${COLLECTIONS_REQ:-'requirements.yml'}
+PYTHON_REQUIREMENTS_FILE=${PYTHON_REQUIREMENTS_FILE:-'requirements.txt'}
+PIP_COMMAND=${PIP_COMMAND:-'pip-3.8'}
+DOWNSTREAM_NS=${DOWNSTREAM_NS:-'redhat'}
+JENKINS_JOBS_DIR=${JENKINS_JOB_DIR:-'/jenkins_jobs'}
+HARMONIA_HOME=${HARMONIA_HOME:-"${WORKSPACE}/harmonia"}
 
 configureAnsible() {
   local path_to_ansible_cfg=${1}
@@ -28,4 +34,138 @@ readValueFromFile() {
   local sep=${3:-':'}
 
   grep -e "${field}" "${file}" | cut "-d${sep}" -f2 | sed -e 's;^ *;;'
+}
+
+ansibleGalaxyCollectionInstall() {
+    local args=${1}
+
+    ansible-galaxy collection install --force ${args}
+}
+
+
+ansibleGalaxyCollectionInstallFromRequirementFile() {
+  local path_to_req=${1:-"${COLLECTIONS_REQ}"}
+
+  # shellcheck disable=SC2086
+  if [ -e "${path_to_req}" ]; then
+    ansibleGalaxyCollectionInstall --force -r "${path_to_req}"
+  fi
+}
+
+ansibleGalaxyCollectionInstallByName() {
+  local collectionName=${1}
+  local exitStatus=${2:-'1'}
+
+  if [ -z "${collectionName}" ]; then
+    echo "Collection name can't be empty."
+    exit ${exitStatus}
+  fi
+  ansibleGalaxyCollectionInstall ${1}
+}
+
+installPythonRequirementsIfAny() {
+  local requirementsFile=${1}
+
+  if [ -n "${requirementsFile}" ]; then
+    echo "Checks if ${requirementsFile} exists..."
+    if [ -e "${requirementsFile}" ]; then
+      echo 'It does, performing required installations.'
+      echo "Install Python dependencies provided in ${requirementsFile}:"
+      "${PIP_COMMAND}" install --user -r "${requirementsFile}"
+      echo 'Done.'
+    else
+      echo 'File does not exists. Skipping.'
+    fi
+  fi
+}
+
+printEnv() {
+  set +u
+  if [ -n "${HERA_DEBUG}" ]; then
+    echo ==========
+    env
+    echo ==========
+  fi
+  set -u
+}
+
+setRequiredEnvVars() {
+  export ANSIBLE_HOST_KEY_CHECKING='False'
+}
+
+checkWorkdirExistsAndSetAsDefault() {
+  local exitStatus=${1:-'1'}
+  if [ ! -d "${WORKDIR}" ]; then
+    echo "WORKDIR ${WORKDIR} does not exists or is not a directory."
+    exit "${exitStatus}"
+  fi
+  cd "${WORKDIR}"
+}
+
+checkWorkspaceIsDefinedAndExists() {
+    local workspace=${1:-"${WORKSPACE}"}
+    local exitUndefined=${2:-'1'}
+    local exitNotDir=${3:-'2'}
+
+    if [ -z "${workspace}" ]; then
+      echo "workspace is undefined, aborting."
+      exit "${exitUndefined}"
+    fi
+
+    if [ ! -d "${workspace}" ]; then
+      echo "workspace is not a directory, aborting."
+      exit "${exitNotDir}"
+    fi
+}
+
+get_last_build_id() {
+  local path_to_permalinks="${1}/permalinks"
+
+  if [ -z "${path_to_permalinks}" ]; then
+    echo "No permalinks file: ${path_to_permalinks}." 1>&2
+    exit 1
+  fi
+
+  # shellcheck disable=SC2155
+  local last_build_id=$(grep -e 'lastSuccessfulBuild' "${path_to_permalinks}" | cut -f2 -d\ )
+  if [ -z "${last_build_id}" ]; then
+    echo "Could not retrieved the id of the last successful build for ${last_build_id}." 1>&2
+    exit 3
+  fi
+  echo "${last_build_id}"
+}
+
+get_path_to_collection_tarball() {
+  local collection_home=${1}
+  local collection_name=${2}
+  local collection_namespace=${3:-'redhat'}
+  local tarball_extension=${4:-'tar.gz'}
+
+  path_to_tarball=$(ls "${collection_home}/${collection_name}/${collection_namespace}-${collection_name}"*."${tarball_extension}")
+  if [ ! -e "${path_to_tarball}" ]; then
+    ls -1 "${collection_home}/${collection_name}/" 1>&2
+    echo "Path to archive does not exits: ${path_to_tarball}." 1>&2
+    exit 1
+  fi
+  echo "${path_to_tarball}"
+}
+
+install_collections() {
+  local collections_to_install=${@}
+
+  if [ -n "${collections_to_install}" ]; then
+    IFS=','
+    read -r -a collections_to_install_array <<< "${collections_to_install}"
+    for collection_to_install in "${collections_to_install_array[@]}"
+    do
+      path_to_builds=${JENKINS_JOBS_DIR}/ansible-janus-${collection_to_install}/builds
+      if [ ! -d "${path_to_builds}" ]; then
+        echo "Invalid path to collection job: ${path_to_builds}."
+        exit 2
+      fi
+      last_build_id=$(get_last_build_id "${path_to_builds}")
+      path_to_collection_archive=$(get_path_to_collection_tarball "${path_to_builds}/${last_build_id}/archive/workdir/downstream/" "${collection_to_install}")
+      ansible-galaxy collection install "${path_to_collection_archive}"
+    done
+  fi
 }
--- ansible/playbook.sh
+++ ansible/playbook.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 set -eo pipefail
 
-if [ ! -d "${WORKDIR}" ]; then
-  echo "WORKDIR ${WORKDIR} does not exists or is not a directory."
-  exit 1
-fi
-
 readonly PLAYBOOK=${PLAYBOOK:-'playbooks/playbook.yml'}
 readonly PATH_TO_PLAYBOOK=${PATH_TO_PLAYBOOK:-"${WORKDIR}/${PLAYBOOK}"}
 readonly SYSTEM_REQ=${SYSTEM_REQ:-'requirements.txt'}
-readonly COLLECTIONS_REQ=${COLLECTIONS_REQ:-'requirements.yml'}
 readonly ANSIBLE_VERBOSITY=${ANSIBLE_VERBOSITY:-''}
 
 set -u
-cd "${WORKDIR}"
+
+source "$(dirname $(realpath ${0}))/common.sh"
+
+checkWorkdirExistsAndSetAsDefault
 
 if [ ! -e "${PATH_TO_PLAYBOOK}" ]; then
   echo "Playbook does not exits: ${PATH_TO_PLAYBOOK}."
@@ -21,8 +18,6 @@ if [ ! -e "${PATH_TO_PLAYBOOK}" ]; then
   exit 2
 fi
 
-if [ -e "${COLLECTIONS_REQ}" ]; then
-  ansible-galaxy collection install -r "${COLLECTIONS_REQ}"
-fi
+ansibleGalaxyCollectionInstallFromRequirementFile
 
 ansible-playbook ${ANSIBLE_VERBOSITY} "${PLAYBOOK}"
